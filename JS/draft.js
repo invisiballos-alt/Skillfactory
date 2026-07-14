ea2bf357418003939a0008a2acfc452be8a1abe7
Dmitry <mr.timy297@yandex.ru>
2026-07-14 13:42:25 +0500

    Fin v 2.0

--- JS/draft.js
+++ JS/draft.js
@@ -1,15 +1,3 @@
-const firstFunction = (num) => {
-  console.log(`Первая функция работает, аргумент равен ${num}`);
-};
-
-const secondFunction = (a) => {
-  console.log("Вторая функция работает");
-  return firstFunction(a);
-};
-
-const thirdFunction = () => {
-  console.log("Третья функция работает");
-  return secondFunction(3);
-};
-
-thirdFunction();
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
